c42ff5aafd6aa600665146690e7fc0bac85f5269
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:56:45 -0800

    config: Disable RescueParty
    RescueParty's aggressive approach to error recovery almost never helps
    with the problems we're likely to face, and its risk of data loss is far
    too high. Force-disable it entirely and remove all other conditions,
    especially those that depend on data integrity in the first place, to
    reduce the risk of RescueParty triggering falsely and causing data loss.
    
    Note that it is still possible to force-enable RescueParty for testing
    purposes using the persist.sys.enable_rescue property.

--- config/common.mk
+++ config/common.mk
@@ -207,6 +207,10 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=true
 
+# Disable RescueParty due to high risk of data loss
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+
 # FRP
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/wipe-frp.sh:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/wipe-frp
