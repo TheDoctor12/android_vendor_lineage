9d419716384fec3a433149169254d3641ccc64a2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-27 20:08:36 +0800

    config: add PERF_ANIM_OVERRIDE flag
    Change-Id: I7d264207abef968739b57c9fce19e8f65466ee8c

--- config/common.mk
+++ config/common.mk
@@ -45,9 +45,12 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
-# Bypass charging
+# Other ROM feature flags
+PERF_ANIM_OVERRIDE ?= false
 BYPASS_CHARGE_SUPPORTED ?= false
+
 PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.activity_anim_perf_override=$(PERF_ANIM_OVERRIDE)
     persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED)
 
 # Enable Material Design 3 Expressive
